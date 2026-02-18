03d342a34dd6f67e751e9501d14dd958dfd4947a
berdy <berdysevartem96@gmail.com>
2026-02-18 14:14:19 +0700

    Исправил кавычки

--- code.py
+++ code.py
@@ -4,7 +4,7 @@ import turtle as t
 import math
 
 def isosceles_trapezoid(x, y, base1, base2, height, color, rotation):
-    '''
+    """
     Function, drawing isosceles_trapezoid
     :param x: lower left corner coordinate x
     :param y: lower left corner coordinate y
@@ -14,7 +14,7 @@ def isosceles_trapezoid(x, y, base1, base2, height, color, rotation):
     :param color: color of trapezoid
     :param rotation: the angle of rotation of the figure by degrees, the angle between Ox and the bottom side of the trapezoid
     :return: None
-    '''
+    """
     t.up()
     t.goto(x, y)
     # lower left corner
@@ -35,7 +35,7 @@ def isosceles_trapezoid(x, y, base1, base2, height, color, rotation):
     p4 = t.pos()
     t.down()
     t.begin_fill()
-    t.color('black', color)
+    t.color("black", color)
     t.goto(p1)
     t.goto(p2)
     t.goto(p3)
@@ -43,8 +43,8 @@ def isosceles_trapezoid(x, y, base1, base2, height, color, rotation):
     t.end_fill()
     t.setheading(0)
 
-def rectangle(x, y, sidea, sideb, color = 'white', rotation = 0):
-    '''
+def rectangle(x, y, sidea, sideb, color = "white", rotation = 0):
+    """
     Function, drawing rectangle
     :param x: lower left corner coordinate x
     :param y: lower left corner coordinate y
@@ -53,7 +53,7 @@ def rectangle(x, y, sidea, sideb, color = 'white', rotation = 0):
     :param color: color of rectangle
     :param rotation: the angle of rotation of the figure by degrees, the angle between Ox and the bottom side of the rectangle
     :return: None
-    '''
+    """
     t.up()
     t.goto(x, y)
     t.down()
@@ -67,10 +67,10 @@ def rectangle(x, y, sidea, sideb, color = 'white', rotation = 0):
         t.left(90)
     t.end_fill()
     t.setheading(0)
-    t.color('black')
+    t.color("black")
 
-def rhomb(x, y, side, angle, color = 'white', rotation = 0):
-    '''
+def rhomb(x, y, side, angle, color = "white", rotation = 0):
+    """
     Function, drawing rhomb
     :param x: lower left corner coordinate x
     :param y: lower left corner coordinate y
@@ -78,7 +78,7 @@ def rhomb(x, y, side, angle, color = 'white', rotation = 0):
     :param color: color of rhomb
     :param rotation: the angle of rotation of the figure by degrees, the angle between Ox and the bottom side of the rhomb
     :return: None
-    '''
+    """
     t.up()
     t.goto(x, y)
     t.down()
@@ -92,10 +92,10 @@ def rhomb(x, y, side, angle, color = 'white', rotation = 0):
         t.left(180 - angle)
     t.end_fill()
     t.setheading(0)
-    t.color('black')
+    t.color("black")
 
 def equilateral_triangle(x, y, side, color, rotation):
-    '''
+    """
     Function, drawing equilateral_triangle
     :param x: lower left corner coordinate x
     :param y: lower left corner coordinate y
@@ -103,12 +103,12 @@ def equilateral_triangle(x, y, side, color, rotation):
     :param color: color of triangle
     :param rotation: the angle of rotation of the figure by degrees, the angle between Ox and the bottom side of the triangle
     :return: None
-    '''
+    """
     t.up()
     t.goto(x, y)
     t.down()
     t.left(rotation)
-    t.color('black', color)
+    t.color("black", color)
     t.begin_fill()
     for _ in range (3):
         t.forward(side)
@@ -117,7 +117,7 @@ def equilateral_triangle(x, y, side, color, rotation):
     t.setheading(0)
 
 def right_triangle (x, y, a, b, color, rotation = 0):
-    '''
+    """
     Function, drawing right_triangle.
     :param x: point between legs coordinate x
     :param y: point between legs coordinate y
@@ -126,12 +126,12 @@ def right_triangle (x, y, a, b, color, rotation = 0):
     :param color: color of triangle
     :param rotation: the angle of rotation of the figure by degrees
     :return: None
-    '''
+    """
     t.up()
     t.setposition(x, y)
     t.left(rotation)
     t.down()
-    t.color('black', color)
+    t.color("black", color)
     t.begin_fill()
     t.forward(a)
     pos_a = t.pos()
@@ -145,37 +145,37 @@ def right_triangle (x, y, a, b, color, rotation = 0):
     t.seth(0)
 
 def circle (x, y, r, color):
-    '''
+    """
     Function, drawing circle.
     :param x: centre point coordinate x
     :param y: centre point coordinate y
     :param r: radius of circle
     :param color: color of circle
     :return: None
-    '''
+    """
     t.up()
     t.setposition(x, y)
     t.down()
-    t.color('black', color)
+    t.color("black", color)
     t.begin_fill()
     t.circle(r)
     t.end_fill()
     t.seth(0)
 
 def square(x, y, a, color, rotation = 0):
-    '''
+    """
     Function, drawing square.
     :param x: left top point coordinate x
     :param y: left top point coordinate y
     :param a: side lenght of square
     :param color: color of square
     :return: None
-    '''
+    """
     t.up()
     t.setposition(x, y)
     t.left(rotation)
     t.down()
-    t.color('black', color)
+    t.color("black", color)
     t.begin_fill()
 
     for _ in range(4):
@@ -186,7 +186,7 @@ def square(x, y, a, color, rotation = 0):
     t.seth(0)
 
 def isosceles_triangle(x, y, a, h, color, rotation = 0):
-    '''
+    """
     Function, drawing isosceles triangle
     
     :param x: left bottom point coordinate x
@@ -196,13 +196,13 @@ def isosceles_triangle(x, y, a, h, color, rotation = 0):
     :param color: color of triangle
     :param rotation: rotation angle in degrees
     :return: None
-    '''
+    """
     t.up()
 
     t.setposition(x, y)
     t.down()
 
-    t.color('black', color)
+    t.color("black", color)
     t.begin_fill()
 
     t.seth(rotation)
@@ -221,15 +221,15 @@ def isosceles_triangle(x, y, a, h, color, rotation = 0):
     t.end_fill()
     t.seth(0)
 
-def crown (x, y, m = 'yellow', g = 'red'):
-    '''
+def crown (x, y, m = "yellow", g = "red"):
+    """
     Function, drawing crown.
     :param x: lower left point coordinate x
     :param y: lower left point coordinate y
     :param m: color of metall of the crown
     :param g: golor of gems in the crown
     :return: None
-    '''
+    """
     right_triangle(-100 + x, 0 + y, 50, 100, m)
     # left upper triangle of crown
     t.left(90)
@@ -258,148 +258,148 @@ def crown (x, y, m = 'yellow', g = 'red'):
     # central gem of crown
 
 def rabbit(x, y):
-    '''
+    """
     Function, drawing rabbit.
-    :param x: coordinate x of rabbit's head
-    :param y: coordinate y of rabbit's head
+    :param x: coordinate x of rabbit"s head
+    :param y: coordinate y of rabbit"s head
     :return: None
-    '''
-    # rabbit's head
-    circle(x, y, 40, 'lavender')
-    # rabbit's left ear
-    equilateral_triangle(x - 10, y + 80, 70, 'lavender', 90)
-    # rabbit's right ear
-    equilateral_triangle(x + 10, y + 80, 70, 'lavender', 30)
-    # rabbit's body
-    equilateral_triangle(x - 100, y - 175, 200, 'yellow green', 0)
-    # rabbit's left leg
-    isosceles_trapezoid(x - 70, y - 195, 60, 30, 20, 'tan', 0)
-    # rabbit's right leg
-    isosceles_trapezoid(x + 15, y - 195, 60, 30, 20, 'tan', 0)
-    # rabbit's left arm
-    circle(x - 55, y - 80, 20, 'lavender')
-    # rabbit's right arm
-    circle(x + 55, y - 80, 20, 'lavender')
+    """
+    # rabbit"s head
+    circle(x, y, 40, "lavender")
+    # rabbit"s left ear
+    equilateral_triangle(x - 10, y + 80, 70, "lavender", 90)
+    # rabbit"s right ear
+    equilateral_triangle(x + 10, y + 80, 70, "lavender", 30)
+    # rabbit"s body
+    equilateral_triangle(x - 100, y - 175, 200, "yellow green", 0)
+    # rabbit"s left leg
+    isosceles_trapezoid(x - 70, y - 195, 60, 30, 20, "tan", 0)
+    # rabbit"s right leg
+    isosceles_trapezoid(x + 15, y - 195, 60, 30, 20, "tan", 0)
+    # rabbit"s left arm
+    circle(x - 55, y - 80, 20, "lavender")
+    # rabbit"s right arm
+    circle(x + 55, y - 80, 20, "lavender")
 
 def detailed_square(x, y):
-    '''
+    """
     Function, drawing square.
     :param x: coordinate x of right upper point
     :param y: coordinate y of right upper point
     :return: None
-    '''
+    """
     h = 150 * 2 ** 0.5 # hypotenuse of the biggest triangle in the square
     # orange left center side triangle
-    right_triangle(x, y, 150, 150, 'orange', rotation=135)
+    right_triangle(x, y, 150, 150, "orange", rotation=135)
     # pink upper side triangle
-    right_triangle(x, y, 150, 150, 'pink', rotation=45)
+    right_triangle(x, y, 150, 150, "pink", rotation=45)
     # rectangle
-    rectangle(x, y, 75, 75, 'orange', rotation=-45)
+    rectangle(x, y, 75, 75, "orange", rotation=-45)
     # yellow upper right triangle (between rectangle and upper side triangle)
-    right_triangle(x + h / 4, y + h / 4, 75, 75, 'yellow', rotation=-45)
+    right_triangle(x + h / 4, y + h / 4, 75, 75, "yellow", rotation=-45)
     # red lower right corner triangle
-    right_triangle(x + h / 2, y - h / 2, h / 2, h / 2, 'red', rotation=90)
+    right_triangle(x + h / 2, y - h / 2, h / 2, h / 2, "red", rotation=90)
     # pink center triangle (between rectangle and orange left side triangle)
-    right_triangle(x, y, 75, 75, 'pink', rotation=-135)
+    right_triangle(x, y, 75, 75, "pink", rotation=-135)
     # green bottom side triangle 1
-    right_triangle(x, y - h / 2, 75, 75, 'green', rotation=45)
+    right_triangle(x, y - h / 2, 75, 75, "green", rotation=45)
     # green bottom side triangle 2
-    right_triangle(x - h / 4, y - h / 4, 75, 75, 'green', rotation=-135)
+    right_triangle(x - h / 4, y - h / 4, 75, 75, "green", rotation=-135)
 
 def sword(x, y):
-    '''
+    """
     Function, drawing sword.
     :param x: lower point coordinate x
     :param y: lower point coordinate y
     :return: None
-    '''
-    rectangle(0 + x, 0 + y, 30, 60, 'black', 0)
+    """
+    rectangle(0 + x, 0 + y, 30, 60, "black", 0)
     # sword handle
-    rhomb(15 + x, (-math.sqrt(1800)) / 2 + y, 30, 90, 'gray', 45)
+    rhomb(15 + x, (-math.sqrt(1800)) / 2 + y, 30, 90, "gray", 45)
     # sword pommel
-    rectangle(-25 + x, 50 + y, 80, 10, 'gray', 0)
+    rectangle(-25 + x, 50 + y, 80, 10, "gray", 0)
     # sword guard
-    rectangle(-5 + x, 60 + y, 40, 220, 'gray', 0)
+    rectangle(-5 + x, 60 + y, 40, 220, "gray", 0)
     # sword blade
-    rectangle(13 + x, 60 + y, 4, 220, 'gray', 0)
-    # sword's dale
-    right_triangle(15 + x, 300 + y, math.sqrt(800), math.sqrt(800), 'gray', 225)
+    rectangle(13 + x, 60 + y, 4, 220, "gray", 0)
+    # sword"s dale
+    right_triangle(15 + x, 300 + y, math.sqrt(800), math.sqrt(800), "gray", 225)
     # the tip of the sword
 
 def fish(x, y):
-    '''
+    """
     Function, drawing square.
-    :param x: coordinate x of figure's rightest point
-    :param y: coordinate y of figure's rightest point
+    :param x: coordinate x of figure"s rightest point
+    :param y: coordinate y of figure"s rightest point
     :return: None
-    '''
-    h = 100 * 2** 0.5 # hypotenuse of triangle (fish's head)
+    """
+    h = 100 * 2** 0.5 # hypotenuse of triangle (fish"s head)
     # fish head
-    right_triangle(0 + x, 0 + y, 100, 100, 'blue', 135)
+    right_triangle(0 + x, 0 + y, 100, 100, "blue", 135)
     r = (100 - h / 2) #radius of circle
-    circle(x - r * 2 ** 0.5, y - r, r, 'white')
+    circle(x - r * 2 ** 0.5, y - r, r, "white")
     # fish lower fin
-    right_triangle(x - h / 2, y - 150, 150, 150, 'black', 90)
+    right_triangle(x - h / 2, y - 150, 150, 150, "black", 90)
     # fish upper fin
-    right_triangle(x - h / 2, y + 150, 150, 150, 'black', 180)
+    right_triangle(x - h / 2, y + 150, 150, 150, "black", 180)
     # fish body
-    rectangle(x - h / 2, y, h / 2, h / 2, 'yellow', 135)
+    rectangle(x - h / 2, y, h / 2, h / 2, "yellow", 135)
     # fish tail
-    right_triangle(x - h / 2 - 100, y, 100, 100, 'black', 135)
-    right_triangle(x - h - 100, y - h / 2, h / 2, h / 2, 'green', 90)
-    right_triangle(x - h - 100, y + h / 2, h / 2, h / 2, 'green', 180)
+    right_triangle(x - h / 2 - 100, y, 100, 100, "black", 135)
+    right_triangle(x - h - 100, y - h / 2, h / 2, h / 2, "green", 90)
+    right_triangle(x - h - 100, y + h / 2, h / 2, h / 2, "green", 180)
 
 def butterfly (x, y):
-    '''
+    """
     Function, drawing butterfly.
-    :param x: coordinate x of figure's head
-    :param y: coordinate y of figure's head
+    :param x: coordinate x of figure"s head
+    :param y: coordinate y of figure"s head
     :return: None
-    '''
+    """
     # body from the head
-    circle(x, y, 20, 'brown')
-    circle(x + 7, y - 35, 18, 'brown')
-    circle(x + 18, y - 65, 16, 'brown')
-    circle(x + 27, y - 91, 14, 'brown')
-    circle(x + 40, y - 112, 12, 'brown')
+    circle(x, y, 20, "brown")
+    circle(x + 7, y - 35, 18, "brown")
+    circle(x + 18, y - 65, 16, "brown")
+    circle(x + 27, y - 91, 14, "brown")
+    circle(x + 40, y - 112, 12, "brown")
     # upper left wing
-    equilateral_triangle(x - 135, y - 80, 140, 'cadet blue', 20)
-    circle(x - 90, y - 10, 18, 'lemon chiffon')
-    circle(x - 50, y - 40, 14, 'lemon chiffon')
+    equilateral_triangle(x - 135, y - 80, 140, "cadet blue", 20)
+    circle(x - 90, y - 10, 18, "lemon chiffon")
+    circle(x - 50, y - 40, 14, "lemon chiffon")
     # upper right wing
-    equilateral_triangle(x + 20, y - 30, 140, 'cadet blue', -5)
-    circle(x + 95, y + 10, 18, 'lemon chiffon')
-    circle(x + 60, y - 20, 14, 'lemon chiffon')
+    equilateral_triangle(x + 20, y - 30, 140, "cadet blue", -5)
+    circle(x + 95, y + 10, 18, "lemon chiffon")
+    circle(x + 60, y - 20, 14, "lemon chiffon")
     # lower left wing
-    equilateral_triangle(x - 90, y - 105, 110, 'cadet blue', -20)
-    circle(x - 27, y - 110, 20, 'lemon chiffon')
+    equilateral_triangle(x - 90, y - 105, 110, "cadet blue", -20)
+    circle(x - 27, y - 110, 20, "lemon chiffon")
     # lower right wing
-    equilateral_triangle(x + 31, y - 32, 110, 'cadet blue', -70)
-    circle(x + 80, y - 90, 20, 'lemon chiffon')
+    equilateral_triangle(x + 31, y - 32, 110, "cadet blue", -70)
+    circle(x + 80, y - 90, 20, "lemon chiffon")
 
 def bird(x, y):
-    '''
+    """
     Function, drawing bird.
     :param x: coordinate x of the left corner of the head
     :param y: coordinate y of the left corner of the head
     :return: None
-    '''
+    """
     #head
-    square(x, y, 70, 'red', 45)
-    circle(x + 50, y - 15, 10, 'black')
-    isosceles_triangle(x, y, 99, 49.5, 'orange', rotation = 270)
+    square(x, y, 70, "red", 45)
+    circle(x + 50, y - 15, 10, "black")
+    isosceles_triangle(x, y, 99, 49.5, "orange", rotation = 270)
     #body
-    square(x + 49.5, y - 49.5, 140, 'red', 45)
-    isosceles_triangle(x + 113, y - 180, 70, 30, 'orange')
+    square(x + 49.5, y - 49.5, 140, "red", 45)
+    isosceles_triangle(x + 113, y - 180, 70, 30, "orange")
     #tail
-    isosceles_triangle(x + 198, y - 100, 100, 50, 'yellow')
-    rhomb(x + 198, y - 100, 60, 30, 'red', -30)
+    isosceles_triangle(x + 198, y - 100, 100, 50, "yellow")
+    rhomb(x + 198, y - 100, 60, 30, "red", -30)
 
 def main():
-    '''
+    """
     Main function.
     :return: None
-    '''
+    """
     t.tracer(0)
     t.hideturtle()
     t.right(90)
